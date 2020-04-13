66391b6d39f4ffd46f2436ceb989d7aa4291b24a
Chris Rowson <christopherrowson@gmail.com>
2020-04-13 20:20:57 +0100

    Update getResources.py
    Added 'last updated time'.

--- getResources.py
+++ getResources.py
@@ -1,12 +1,14 @@
 import json
 import urllib.request
-import dateutil.parser
+from datetime import datetime
 import re
 
 resourceURL = "https://opendata.hullcc.gov.uk/dataset/30fd3969-556d-4eae-ae4c-f3f9d2cfa9e3/resource/90e1cce0-295e-4fa7-aa21-ebc4f3e8e8d4/download/scoot_loop_resources_full.json"
 data = urllib.request.urlopen(resourceURL)
 jsonData = json.loads(data.read())
 
+now = datetime.now()
+today = now.strftime("%d/%m/%Y %H:%M:%S")
 strTable = """<style type=\"text/css\">
 .tg  {border-collapse:collapse;border-spacing:0;}
 .tg td{font-family:Arial, sans-serif;font-size:14px;padding:5px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
@@ -14,7 +16,7 @@ strTable = """<style type=\"text/css\">
 .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
 .tg .tg-xkfo{background-color:#000000;border-color:inherit;text-align:left;vertical-align:top;font-weight:bold;color:FFFFFF}
 </style><html>
-<table class =\"tg\" border = \"1\"><tr><th class=\"tg-xkfo\">Hull Scoot Loop Vehicle Flow Data</th></tr>"""
+<table class =\"tg\" border = \"1\"><tr><th class=\"tg-xkfo\">Hull Scoot Loop Vehicle Flow Data (last updated %(now)s)</th></tr>""" %{'now' : today}
 for e in jsonData:
     desc = re.sub("(- https.*$)", "\1", e["description"])
     strRow = "<tr><td class=\"tg-0pky\"><a href=\"" + e["resource_id"] + ".html\">" + desc + "</a></td></tr>"
